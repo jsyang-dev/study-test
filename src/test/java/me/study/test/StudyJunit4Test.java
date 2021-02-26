b42f3ee55e3faa526a26b58efd0d55b1e933f992
Jeongsu Yang <mycat83@gmail.com>
2021-02-26 22:46:16 +0900

    JUnit 5 마이그레이션

---
+++ src/test/java/me/study/test/StudyJunit4Test.java
@@ -0,0 +1,22 @@
+package me.study.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyJunit4Test {
+
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
+
+    @Test
+    public void createTest() {
+        System.out.println("test");
+    }
+
+    @Test
+    public void createTest2() {
+        System.out.println("test2");
+    }
+}
