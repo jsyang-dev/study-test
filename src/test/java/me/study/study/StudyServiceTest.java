cb27676d92829b7e10ff53c616ec50dbe338c2f1
Jeongsu Yang <mycat83@gmail.com>
2021-03-01 23:08:02 +0900

    Mock 객체 Stubbing

--- src/test/java/me/study/study/StudyServiceTest.java
+++ src/test/java/me/study/study/StudyServiceTest.java
@@ -1,12 +1,20 @@
 package me.study.study;
 
+import me.study.domain.Member;
 import me.study.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -18,12 +26,39 @@ class StudyServiceTest {
     StudyRepository studyRepository;
 
     @Test
-    void createStudyService() {
-//        MemberService memberService =  mock(MemberService.class);
-//        StudyRepository studyRepository = mock(StudyRepository.class);
-
+    void createNewStudy() {
         StudyService studyService = new StudyService(memberService, studyRepository);
-
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("test@email.com");
+
+        when(memberService.findById(any())).thenReturn(Optional.of(member));
+
+        assertEquals(member.getEmail(), memberService.findById(1L).get().getEmail());
+        assertEquals(member.getEmail(), memberService.findById(2L).get().getEmail());
+
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+
+        assertThrows(IllegalArgumentException.class, () -> memberService.validate(1L));
+        memberService.validate(2L);
+    }
+
+    @Test
+    void createNewStudy2() {
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("test@email.com");
+
+        when(memberService.findById(any())).thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty());
+
+        assertEquals(member.getEmail(), memberService.findById(1L).get().getEmail());
+
+        assertThrows(RuntimeException.class, () -> memberService.findById(2L));
+
+        assertEquals(Optional.empty(), memberService.findById(3L));
     }
 }
